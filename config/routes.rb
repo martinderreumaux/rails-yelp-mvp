27ccfddc3abde84b28f9ed80af70321a9c4d0131
martin derreumaux <martin.derreumaux.bba@edhec.com>
2019-11-13 15:48:47 +0100

    exercice2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+    # collection do                       # collection => no restaurant id in URL
+    #   get 'top'                         # RestaurantsController#top
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0320112020',
+    category:     'chinese',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0320112021',
+    category:     'italian',
+  },
+  {
+    name:         'Royal au bar',
+    address:      '59A Shoreditch High St, London E1 6PQ',
+    phone_number: '0320112022',
+    category:     'japanese',
+  },
+  {
+    name:         'Chez oam',
+    address:      '59 rue de lille, Belgium',
+    phone_number: '0320112023',
+    category:     'french',
+  },
+  {
+    name:         'le poullailer',
+    address:      '59 avenue du peuple belge, London E1 6PQ',
+    phone_number: '0320112024',
+    category:     'belgian',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
